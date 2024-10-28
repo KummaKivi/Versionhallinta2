7fea68b612c6fa57b6f26e8ab97a5484c082301c
KummaKivi <danielnystrom7@gmail.com>
2024-10-28 15:16:48 +0200

    Random number generator

---
+++ Random Numbers/src/App.java
@@ -0,0 +1,15 @@
+import java.util.Random;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Random random = new Random();
+
+        int randomNumber;
+
+        randomNumber = random.nextInt(5);
+        System.out.println("Random number between 0-4: " + randomNumber);
+
+        randomNumber = random.nextInt(10)+11;
+        System.out.println("Random number between 10-20: " + randomNumber);
+    }
+}
